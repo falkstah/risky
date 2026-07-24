ed044f8545c2364ac0d56326cdc01fa2462e52c7
falkstah <stahmann.falk@gmail.com>
2026-07-24 22:19:13 +0200

    delta

--- trading_logic.py
+++ trading_logic.py
@@ -9,8 +9,9 @@ def calculate_all(params: TradeParameters):
     # 1) Directional basics
     params.dirsign = calculate_dirsign(params)
     params.sl_delta = calculate_SL_delta(params)
-    if params.sl_delta == 0:  # this would lead to division by zero in the following calculations
-        raise ValueError("SL_delta = 0")
+    if params.p_entry is None or params.p_SL is None:
+      if params.sl_delta == 0:  # this would lead to division by zero in the following calculations
+          raise ValueError("SL_delta = 0")
 
     params.rel_risk = calculate_rel_risk(params)
     params.current_direction = get_trade_direction(params)
