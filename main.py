679065cf87e7a8bb526d8034bfce9e327ec9163a
falkstah <stahmann.falk@gmail.com>
2026-07-26 11:01:16 +0200

    keys

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ import streamlit_visualization
 
 
 def main():
+    #init keys:
+    if "key" not in st.session_state:
+        st.session_state["key"] = 0
+
     st.title("Too_Risky - Crypto live lvg and liquidation manager")
     st.text("Opimized for execution speed.")
 
@@ -13,6 +17,7 @@ def main():
     params = calculate_all(params)
 
     #st.session_state["params"] prevents overwriting after calculate_all()
+    st.session_state["params"] = params
     streamlit_visualization.current_direction_label(st.session_state["params"].current_direction)
     streamlit_visualization.fast_order_table(st.session_state["params"])
     streamlit_visualization.visualize_trade(st.session_state["params"])
