1d84ec6d44191c2427ccef2fc180a383f1fc634f
falkstah <stahmann.falk@gmail.com>
2026-07-26 06:47:12 +0200

    lvg vis

--- streamlit_visualization.py
+++ streamlit_visualization.py
@@ -64,7 +64,7 @@ def fast_order_table(params: TradeParameters):
         
         # Wir nutzen Spalten für eine saubere Anordnung nebeneinander
         col1, col2, col3, col4 = st.columns(4)
-        col1.metric("lvg", f"{round(params.lvg, 0)} x")
+        col1.metric("lvg", f"{params.lvg} x")
         col2.metric("isolated margin", f"{round(params.initial_margin, 2)} $")
         col3.metric("p_liquidation", f"{round(params.p_liquidation, 2)} $")
         col4.metric("n_pos_value", f"{round(params.n_pos_value, 2)} $")
