74c8db1dd15f12fcb81c2ce7b08ece3a7587ed82
falkstah <stahmann.falk@gmail.com>
2026-07-29 19:27:19 +0200

    max

--- streamlit_visualization.py
+++ streamlit_visualization.py
@@ -64,8 +64,8 @@ def get_trade_parameters(trade) -> Trade_Parameters: #forces Object of Type Trad
             t.liq_delta_to_SL_delta_ratio = st.number_input("liq_delta_to_SL_delta_ratio: ", value = t.liq_delta_to_SL_delta_ratio, min_value = 1.50, step = 0.25, key = "input_liq_delta_to_SL_delta_ratio")
             t.maintainance_margin_rate=st.number_input("maintainance_margin_rate: ", value = t.maintainance_margin_rate,  min_value = 0.0, step = 0.001, key = "input_maintainance_margin_rate")
             t.maintainance_deduction=st.number_input("maintainance_deduction: ", value = t.maintainance_deduction, min_value = 0.0, step = 0.001, key = "input_maintainance_deduction")
-            t.total_max_lvg = st.number_input("total_max_lvg: ", value = t.total_max_lvg, min_value = 0.0, step = 0.5, key = "input_total_max_lvg")
-            t.total_max_margin = st.number_input("total_max_margin: ", value = t.total_max_margin if t.total_max_lvg >= 0.0 else 0.0, min_value = 1.0, step = 1.0, key ="input_total_max_margin")
+            t.total_max_lvg = st.number_input("total_max_lvg: ", value = t.total_max_lvg if t.total_max_lvg >= 0 else 0.0, min_value = 0.0, step = 0.5, key = "input_total_max_lvg")
+            t.total_max_margin = st.number_input("total_max_margin: ", value = t.total_max_margin if t.total_max_margin >= 0.0 else 0.0, min_value = 1.0, step = 1.0, key ="input_total_max_margin")
 
             t.trailing_SL_percent = st.number_input(
                 "Trailing SL (%):",
