c5e083887c3ba4d2e49521387815e16420d6e3c9
falkstah <stahmann.falk@gmail.com>
2026-08-05 20:22:09 +0200

    im

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ import sys
 import os
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
-import streamlit as st
-
 from classes import Trade, Trade_Parameters, Tranche, Tranche_Parameters, Take_Profit_Target, Entry_Level
 from trading_logic import calculate_all
 import streamlit_visualization
