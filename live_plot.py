de92d67085eb3806d9a5241799a8c692e50c6018
falkstah <stahmann.falk@gmail.com>
2026-08-14 22:10:57 +0200

    menu

--- live_plot.py
+++ live_plot.py
@@ -141,6 +141,11 @@ def update_chart(_, timeframe):
     # Unsichtbarer Punkt für Platz rechts, to draw entry lines
     extra_time = df["t"].max() + pd.Timedelta(minutes=10)
 
+    #drag and drop menu:
+    entry_price = df["close"].iloc[-1]
+    tp_price = entry_price * 1.01
+    sl_price = entry_price * 0.99
+
     fig.add_trace(
         go.Scatter(
             x=[extra_time],
@@ -157,7 +162,41 @@ def update_chart(_, timeframe):
         template="plotly_dark",
         height=None,
         autosize=True,
-        margin=dict(l=0, r=0, t=0, b=0)
+        margin=dict(l=0, r=0, t=0, b=0),
+
+        #drag menu
+        shapes=[
+            # ENTRY
+            dict(
+                type="line",
+                x0=df["t"].min(),
+                x1=df["t"].max() + pd.Timedelta(minutes=30),
+                y0=entry_price,
+                y1=entry_price,
+                line=dict(color="blue", width=2),
+                editable=True
+            ),
+            # TP
+            dict(
+                type="line",
+                x0=df["t"].min(),
+                x1=df["t"].max() + pd.Timedelta(minutes=30),
+                y0=tp_price,
+                y1=tp_price,
+                line=dict(color="green", width=2),
+                editable=True
+            ),
+            # SL
+            dict(
+                type="line",
+                x0=df["t"].min(),
+                x1=df["t"].max() + pd.Timedelta(minutes=30),
+                y0=sl_price,
+                y1=sl_price,
+                line=dict(color="red", width=2),
+                editable=True
+            )
+        ]
     )
 
     return fig
