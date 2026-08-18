8867cc74d9c8f8c1b6ea95393f7e3016cb9f472b
falkstah <stahmann.falk@gmail.com>
2026-08-18 12:18:53 +0200

    platz

--- sources/services/loader.py
+++ sources/services/loader.py
@@ -3,5 +3,11 @@ import pandas as pd
 
 def load_data():
     # Beispiel: CSV laden
-    df = pd.read_csv("data/input.csv")
+    #df = pd.read_csv("data/input.csv")
+    
+    # Platzhalter-Daten, damit Dash startet
+    df = pd.DataFrame({
+        "x": [1, 2, 3, 4, 5],
+        "y": [10, 15, 13, 17, 20]
+    })
     return df
