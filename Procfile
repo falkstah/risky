01ec2b318dcf2af291a64de2d9cb92139dd44f13
falkstah <stahmann.falk@gmail.com>
2026-08-18 12:27:23 +0200

    process

--- Procfile
+++
@@ -1,6 +0,0 @@
-# sources/processor.py
-def process_data(df):
-    # Beispiel: Spalten umbenennen und filtern
-    df = df.rename(columns={"old_name": "new_name"})
-    df = df[df["new_name"] > 0]
-    return df
--- sources/services/processor.py
+++ sources/services/processor.py
@@ -1,6 +1,9 @@
 # sources/processor.py
 def process_data(df):
     # Beispiel: Spalten umbenennen und filtern
-    df = df.rename(columns={"old_name": "new_name"})
-    df = df[df["new_name"] > 0]
+    #df = df.rename(columns={"old_name": "new_name"})
+    #df = df[df["new_name"] > 0]
+    
+    # Platzhalter: gib die Daten einfach unverändert zurück
+    print("Processor läuft (Platzhalter).")
     return df
