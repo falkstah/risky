794720eeb294333666324848b544dd9e091b45e1
falkstah <stahmann.falk@gmail.com>
2026-08-18 20:07:19 +0200

    ge

--- app/ui_init.py
+++ app/ui_init.py
@@ -7,7 +7,8 @@ server = Flask(__name__)
 server.config["SECRET_KEY"] = os.environ.get("SECRET_KEY", "replace-with-secure-key")
 
 # socketio optional, falls du es brauchst
-socketio = SocketIO(server, async_mode= "gevent", cors_allowed_origins="*")
+#ggfs. asynch_mode = gevent erzwingen
+socketio = SocketIO(server, cors_allowed_origins="*")
 
 # Dash app gebunden an Flask server
 #the type ignore is used to ignore th arg error that is a bug?
