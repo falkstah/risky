cfac4eb38333bbd1797e79ec0f05699dd5be1465
falkstah <stahmann.falk@gmail.com>
2026-08-21 16:09:26 +0200

    plotly

--- assets/0_libs.js
+++ assets/0_libs.js
@@ -1,6 +1,11 @@
-/*0_ in name bc has to be loaded first. makes io globally available:*/
-// Socket.IO‑Client dynamisch laden
-const script = document.createElement("script");
-script.src = "https://cdn.socket.io/4.7.2/socket.io.min.js";
-script.onload = () => console.log("Socket.IO‑Client geladen");
-document.head.appendChild(script);
+// Socket.IO-Client laden
+const socketioScript = document.createElement("script");
+socketioScript.src = "https://cdn.socket.io/4.7.2/socket.io.min.js";
+document.head.appendChild(socketioScript);
+
+// Plotly laden
+const plotlyScript = document.createElement("script");
+plotlyScript.src = "https://cdn.plot.ly/plotly-latest.min.js";
+document.head.appendChild(plotlyScript);
+
+plotlyScript.onload = () => console.log("Plotly geladen");
